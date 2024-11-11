d6ee9c2ceff55fb160db5b1c028034f894b4aadf
Namhehe237 <namnguyen23072003@gmail.com>
2024-11-11 17:11:46 +0700

    a

--- app/src/main/java/com/nguyenthanhnam/game/config/GameConfig.java
+++ app/src/main/java/com/nguyenthanhnam/game/config/GameConfig.java
@@ -4,7 +4,7 @@ public class GameConfig {
     public static final int GRID_SIZE = 15;
     public static final int GAME_FPS = 10;
     public static final long FRAME_TIME = 1000 / GAME_FPS;
-    public static final int DEFAULT_LIVES = 3;
+    public static final int DEFAULT_LIVES = 1;
     public static final int DEFAULT_BOMB_CAPACITY = 1;
     public static final int BOMB_TIMER = 30;
     public static final int EXPLOSION_TIMER = 5;
--- app/src/main/java/com/nguyenthanhnam/game/entity/Bomb.java
+++ app/src/main/java/com/nguyenthanhnam/game/entity/Bomb.java
@@ -8,7 +8,7 @@ public class Bomb extends GameObject {
     public Bomb(float x, float y) {
         super(x, y);
         this.timer = 30; // 3 seconds at 10 fps
-        this.explosionRange = 1;
+        this.explosionRange = 3;
     }
 
     public void tick() {
